57139b3a18055c256b8d75b740312ba5e1a10363
Divya <beingdivyadixit@gmail.com>
2023-01-14 13:20:05 +0530

    Welcome Program

---
+++ Day20UserRegistrationMsTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day20UserRegistrationMsTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Day-20 User Registration MS test");
+            Console.ReadLine();
+        }
+    }
+}
