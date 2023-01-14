85b892ad0db29cc4c7d12aeef111a5fd7a618f73
Divya <beingdivyadixit@gmail.com>
2023-01-14 15:31:45 +0530

    Added for MS test for Last Name UC2

--- Day20UserRegistrationMsTest/RegularExpression.cs
+++ Day20UserRegistrationMsTest/RegularExpression.cs
@@ -14,6 +14,12 @@ namespace Day20UserRegistrationMsTest
             string pattern = "^[A-Z]{1}[a-z]{2,}$";
              return TestPattern(pattern, inputs);
         }
+        //Created last Name method-UC2
+        public string lastName(string inputs)
+        {
+            string pattern = "^[A-Z]{1}[a-z]{2,}$";
+            return TestPattern(pattern, inputs);
+        }
         //Created Test pattern method
         public  string TestPattern(string pattern, string inputs)
         {
--- UserRegistrationMStest/RegularExpressionTest.cs
+++ UserRegistrationMStest/RegularExpressionTest.cs
@@ -25,5 +25,21 @@ namespace UserRegistrationMStest
             //Assert
             Assert.AreEqual(expected,actual);
         }
+        [TestMethod]
+        [TestCategory("Last Name")]
+        [DataRow("Dixit", "valid")]
+        [DataRow("dixit", "Invalid")]
+        public void Should_Return_lastName_Is_Valid_Or_Not(string inputs, string expected)
+        {   //AAA Methodology
+            //Arrange
+            RegularExpression regularExpression = new RegularExpression();
+
+            //Act
+            string actual = regularExpression.lastName(inputs);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
     }
 }
