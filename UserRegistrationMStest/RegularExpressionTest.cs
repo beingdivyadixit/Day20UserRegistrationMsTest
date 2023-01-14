224b03260a2b855788f9fca9ae419d6a9fc0fc7b
Divya <beingdivyadixit@gmail.com>
2023-01-14 16:15:26 +0530

    Updated to MS Test of password rule-4 program UC8

--- UserRegistrationMStest/RegularExpressionTest.cs
+++ UserRegistrationMStest/RegularExpressionTest.cs
@@ -72,7 +72,7 @@ namespace UserRegistrationMStest
         }
         [TestMethod]
         [TestCategory("Password")]
-        [DataRow("Abcdofo8", "valid")]
+        [DataRow("Kjhfld@68", "valid")]
         [DataRow("kjhfgdh1", "Invalid")]
         public void Should_Return_password_Is_Valid_Or_Not(string inputs, string expected)
         {   //AAA Methodology
